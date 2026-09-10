65448d62bfc6da8fe69e9f427b4b772f73b15f27
terrytam-2627 <terrytan636@gmail.com>
2026-09-10 07:47:46 +0000

    [skip actions] lab/lab01/ch01_t05_variables.py

--- lab/lab01/ch01_t04_handling_errors.py
+++ lab/lab01/ch01_t04_handling_errors.py
@@ -1,3 +1,4 @@
 print ("How do you make a hot dog stand?")
-print ("You take away its chair!")
+print ("You take away its chair!") 
+
                                                                                                 
