5d01d55457344dc01eec45d08c8a3219217fba22
terrytam-2627 <terrytan636@gmail.com>
2026-09-10 08:53:12 +0000

    lab/lab01/ch01_t14_review.py

--- lab/lab01/ch01_t14_review.py
+++ lab/lab01/ch01_t14_review.py
@@ -4,6 +4,6 @@ exercises_completed = 13
 points_per_exercise = 5
 
 point_total = 100
-point_total += exercises_completed + points_per_exercise 
+point_total += exercises_completed * points_per_exercise 
 
 
