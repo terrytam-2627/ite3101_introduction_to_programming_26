dbae8588853af36480f98139e12f847223ec665f
terrytam-2627 <terrytan636@gmail.com>
2026-09-10 08:29:01 +0000

    [skip actions] lab/lab01/ch01_t11_multi_line_strings.py

--- lab/lab01/ch01_t09_numbers.py
+++ lab/lab01/ch01_t09_numbers.py
@@ -1,6 +1,7 @@
 cucumbers = 1
 price_per_cucumber = 3.25
-total_cost = cucumbers * price_per_cucumber
+total_cost = cucumbers * price_per_cucumber 
+
 
 
  
--- lab/lab01/ch01_t11_multi_line_strings.py
+++ lab/lab01/ch01_t11_multi_line_strings.py
@@ -1,3 +1,3 @@
 haiku =""""The old pond,
-A frog 
+A frog jumps in:
 """
