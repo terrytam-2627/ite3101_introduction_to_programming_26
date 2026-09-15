a0bc47bbfb2039f953f306122276a3b927cd0ff6
terrytam-2627 <terrytan636@gmail.com>
2026-09-15 13:07:07 +0000

    [skip actions] lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -1,4 +1,4 @@
-caeser =# Assign your variables below, each on its own line!
+caeser ="# Assign your variables below, each on its own line!
 
 
 # Put your variables above this line, and uncomment the following 3 line of codes.
