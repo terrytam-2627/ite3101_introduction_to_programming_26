0574df16da40daccc4335e2fcdcbe5bf3f2d0097
terrytam-2627 <terrytan636@gmail.com>
2026-09-16 12:22:59 +0000

    lab/lab02/ch02_t14_string_formatting_with_percentage_1.py

--- lab/lab02/ch02_t14_string_formatting_with_percentage_1.py
+++ lab/lab02/ch02_t14_string_formatting_with_percentage_1.py
@@ -1,4 +1,5 @@
 string_1 = "Camelot"
 string_2 = "place"
 
-print("Let's not go to %s. 'Tis a silly %s." % (string_1, string_2))
+print("Let's not go to %s. 'Tis a silly %s." % (string_1, string_2)) 
+
