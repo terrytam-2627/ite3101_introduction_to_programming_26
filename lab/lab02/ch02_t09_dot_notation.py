91d92910a27076e6a2e522838c4ae124f4f8f903
terrytam-2627 <terrytan636@gmail.com>
2026-09-16 11:59:07 +0000

    [skip actions] lab/lab02/ch02_t10_printing_strings.py

--- lab/lab02/ch02_t09_dot_notation.py
+++ lab/lab02/ch02_t09_dot_notation.py
@@ -2,3 +2,4 @@ ministry = "The Ministry of Silly Walks"
 
 print(len(ministry))
 print(ministry.upper())
+ 
--- lab/lab02/ch02_t10_printing_strings.py
+++ lab/lab02/ch02_t10_printing_strings.py
@@ -1,2 +1,4 @@
 """Tell Python to print "Monty Python"
 to the console on line 4!"""
+
+print("")
