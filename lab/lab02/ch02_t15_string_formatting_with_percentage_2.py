a7c27a20a895bc0ee98d58bfe27ab5c285e35131
terrytam-2627 <terrytan636@gmail.com>
2026-09-16 12:26:39 +0000

    [skip actions] lab/lab02/ch02_t15_string_formatting_with_percentage_2.py

--- lab/lab02/ch02_t15_string_formatting_with_percentage_2.py
+++ lab/lab02/ch02_t15_string_formatting_with_percentage_2.py
@@ -2,6 +2,6 @@ name = input("What is your name? ")
 quest = input("What is your quest? ")
 color = input("What is your favorite color? ")
 
-# Uncomment the below 2 line of code!
-# print("Ah, so your name is %s, your quest is %s, "
-# "and your favorite color is %s." % (name, quest, color)) 
+ Uncomment the below 2 line of code!
+ print("Ah, so your name is %s, your quest is %s, "
+ "and your favorite color is %s." % (name, quest, color)) 
