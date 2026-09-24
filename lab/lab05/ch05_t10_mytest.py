5d65c88dafca15b4eed6101aa919ee5cd3ef3262
terrytam-2627 <terrytan636@gmail.com>
2026-09-24 07:42:41 +0000

    lab/lab05/ch05_t10_mytest.py

--- lab/lab05/ch05_t10_mytest.py
+++ lab/lab05/ch05_t10_mytest.py
@@ -7,5 +7,7 @@ if len(original) > 0 and original.isalpha():
     first = word[0]
     new_word = word + first + pyg
     new_word = new_word[1:len(new_word)]
+    print(new_word)
 else:
     print('empty')
+    
