e0be72a03642d2c8182c0e8999dedb069b5b7c8e
terrytam-2627 <terrytan636@gmail.com>
2026-09-24 07:39:09 +0000

    lab/lab05/ch05_t08_move_it_back.py

--- lab/lab05/ch05_t08_move_it_back.py
+++ lab/lab05/ch05_t08_move_it_back.py
@@ -5,6 +5,6 @@ original = input('Enter a word:')
 if len(original) > 0 and original.isalpha():
     word = original.lower()
     first = word[0]
-    new word = word + first + pyg
+    new_word = word + first + pyg
 else:
     print("empty") 
