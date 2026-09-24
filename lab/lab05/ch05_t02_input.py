b8027e389a8a3fbfdbd3dd674d632cc031ed74d3
terrytam-2627 <terrytan636@gmail.com>
2026-09-24 07:15:02 +0000

    [skip actions] lab/lab05/ch05_t03_check_yourself.py

--- lab/lab05/ch05_t02_input.py
+++ lab/lab05/ch05_t02_input.py
@@ -1,4 +1,4 @@
 print('Welcome to the Pig Latin Translator!')
 
 # Start coding here!
-original = input("")
+original = input("Enter a word: ")
--- lab/lab05/ch05_t03_check_yourself.py
+++ lab/lab05/ch05_t03_check_yourself.py
@@ -2,3 +2,5 @@ print('Welcome to the Pig Latin Translator!')
 
 # Start coding here!
 original = input("Enter a word:")
+
+if len(orininal)
