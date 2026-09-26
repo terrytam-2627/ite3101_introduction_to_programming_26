e6c4ce5c63811ea1f50f61afdacc75b3f3b9f048
terrytam-2627 <terrytan636@gmail.com>
2026-09-26 07:24:26 +0000

    [skip actions] lab/lab06/ch06_t17_review_functions.py

--- lab/lab06/ch06_t17_review_functions.py
+++ lab/lab06/ch06_t17_review_functions.py
@@ -1,6 +1,6 @@
 def shut_down (s: str) -> str:
     if s == "yes":
-       return "Shutting down"
+       return "shutting down"
     if s == "no":
          return "shutdown aborted"
     return "Sorry"
