99a8449159df9703d90037152b5d54a795094fe7
terrytam-2627 <terrytan636@gmail.com>
2026-09-26 07:32:46 +0000

    [skip actions] lab/lab06/ch06_t19_builtin_functions.py

--- lab/lab06/ch06_t19_builtin_functions.py
+++ lab/lab06/ch06_t19_builtin_functions.py
@@ -1,7 +1,7 @@
 from typing import Any
 
-def distance_from_zero(d: Any)->:Any
+def distance_from_zero(d: Any)-> Any
     if type(d) == int or type(d) == float:
         return abs(d)
     else:
-        return "nope"
+        return "Nope"
